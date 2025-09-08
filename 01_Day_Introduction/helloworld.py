257f75a9d344258c0190a487cbc413002f1f2b31
MightyShambel <shambelmighty51@gmail.com>
2025-09-08 16:11:38 +0200

    Fix string concatenation and complex literal; add type demos

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -21,3 +21,33 @@ print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
 print(type(3 == 3))              # Bool
 print(type(3 >= 3))              # Bool
+
+
+#This is arithemetic operators
+# sept 8 2025
+
+# addition
+print("addition of 3 and 3 is", 3+3)
+# subtraction
+print("subtraction of 6 and 3 is", 6-3)
+# multiplication 
+print("multiplication of 1 and 3 is", 1*3)
+# division
+print("division of 9 and 3 is", 9/3)
+#modulus
+print("modulus of 10 and 7 is", 10%7)
+#exponential
+print("exponential of 3 and 1 is", 3**1)
+#Floor division operator
+print("floor division of 10 and 3 is", 10//3)
+
+#Displaying the type
+print("Displaying Integer",type(3))
+print("Displaying Float ",type(3.0))
+print("Displaying set ",type({"Mighty",25,"Venice",True}))
+print("Displaying tuple ",type(("Mighty",1.75,52)))
+print("Displaying List ",type([1,2]))
+print("Displaying Complex ",type(1+2j))
+print("Displaying strings", type("Mighty"))
+print("Display true",type(3==3))
+print("Display false",type(3!=4))
