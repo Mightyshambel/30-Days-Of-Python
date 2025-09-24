6417e8a5a10dd5a8866fcb08f7e0a2a4ca2b8625
MightyShambel <shambelmighty51@gmail.com>
2025-09-24 16:48:29 +0200

    Day 5 lists: fix bugs and add exercises solutions (append/insert/join/slice/reverse)

--- 05_Day_Lists/day_5.py
+++ 05_Day_Lists/day_5.py
@@ -106,7 +106,7 @@ print(fruits)       # ['orange', 'mango', 'lemon']
 del fruits[1]     
 print(fruits)       # ['orange', 'lemon']
 del fruits
-print(fruits)       # This should give: NameError: name 'fruits' is not defined
+# print(fruits)       # This would raise: NameError: name 'fruits' is not defined
 
 # clear
 fruits = ['banana', 'orange', 'mango', 'lemon']
@@ -177,3 +177,90 @@ ages.sort()
 print(ages) 
 ages.sort(reverse=True)
 print(ages) 
+
+
+## 💻 Exercises: Day 5
+
+### Exercises: Level 1
+
+# 1. Declare an empty list
+empty_list = list()
+# 2. Declare a list with more than 5 items
+days =["Moday","Tuesday","Wednsday","Thursday","Friday","Saturday","Sunday"]
+print(days)
+# 3. Find the length of your list
+print(len(days))
+# 4. Get the first item, the middle item and the last item of the list
+print(days[::2])
+# 5. Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+mixed_data_types =["Mighty",25,5,2,"Single","Venice"]
+print(mixed_data_types)
+# 6. Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+it_companies =["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle","Amazon"]
+
+# 7. Print the list using _print()_
+print(it_companies)
+# 8. Print the number of companies in the list
+print(len(it_companies))
+# 9. Print the first, middle and last company
+print(it_companies[::2])
+# 10. Print the list after modifying one of the companies
+it_companies[3] = "Rely Bytes"
+print(it_companies)
+# 11. Add an IT company to it_companies
+it_companies.append("X")
+print(it_companies)
+# 12. Insert an IT company in the middle of the companies list
+it_companies.insert(3,"OpenAI")
+# 13. Change one of the it_companies names to uppercase (IBM excluded!)
+print(it_companies[3].upper())
+# 14. Join the it_companies with a string '#;&nbsp; '
+print('#;&nbsp; '.join(it_companies))
+# 15. Check if a certain company exists in the it_companies list.
+print("Melograno" in it_companies)# 16. Sort the list using sort() method
+# 17. Reverse the list in descending order using reverse() method
+print(it_companies.reverse())
+# 18. Slice out the first 3 companies from the list
+print(it_companies[3:])
+# 19. Slice out the last 3 companies from the list
+print(it_companies)
+print(it_companies[0:-3])
+# 20. Slice out the middle IT company or companies from the list
+print(it_companies)
+print("Slice out the middle",it_companies[:(len(it_companies))//2:])
+
+# 21. Remove the first IT company from the list
+# 22. Remove the middle IT company or companies from the list
+# 23. Remove the last IT company from the list
+# 24. Remove all IT companies from the list
+# 25. Destroy the IT companies list
+# 26. Join the following lists:
+"""
+    front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+    back_end = ['Node','Express', 'MongoDB']
+"""
+
+#27. After joining the lists in question 26. Copy the joined list and assign it to a variable full_stack, then insert Python and SQL after Redux.
+
+### Exercises: Level 2
+
+# 1. The following is a list of 10 students age
+
+"""
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+- Sort the list and find the min and max age
+- Add the min age and the max age again to the list
+- Find the median age (one middle item or two middle items divided by two)
+- Find the average age (sum of all items divided by their number )
+- Find the range of the ages (max minus min)
+- Compare the value of (min - average) and (max - average), use _abs()_ method
+
+1. Find the middle country(ies) in the [countries list](https://github.com/Asabeneh/30-Days-Of-Python/tree/master/data/countries.py)
+1. Divide the countries list into two equal lists if it is even if not one more country for the first half.
+1. ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']. Unpack the first three countries and the rest as scandic countries.
+
+🎉 CONGRATULATIONS ! 🎉
+
+[<< Day 4](../04_Day_Strings/04_strings.md) | [Day 6 >>](../06_Day_Tuples/06_tuples.md)
+"""
